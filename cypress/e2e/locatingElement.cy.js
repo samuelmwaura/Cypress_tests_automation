64e7dd1cbac00fb26527f8d59384323ca5c4bfe1
Samuel <samuelmwaura081@gmail.com>
2023-07-27 19:27:54 +0300

    [Add] Locate an element and assert its property

--- cypress/e2e/locatingElement.cy.js
+++ cypress/e2e/locatingElement.cy.js
@@ -1,11 +1,9 @@
 describe('Show elements locator', ()=>{
     it('First Test case',()=>{
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/') // Visit URL
+        cy.get('.search-keyword').type('ca') //Identify an entry field
+        cy.wait(5000)  //Wait for the results to load.
+        cy.get('.product:visible').should("have.length",4) // Assert on the the length property for the selected element.
     })
 
-    it('Second testcase',()=>{
-        
-    })
-
-    
 })  
