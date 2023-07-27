480ad1ed875589b7b67d971830be2cc736d71ea4
Samuel <samuelmwaura081@gmail.com>
2023-07-27 12:46:05 +0300

    [ADD] Adding the .env and structuting the first test

---
+++ cypress/e2e/locatingElement.cy.js
@@ -0,0 +1,11 @@
+describe('Show elements locator', ()=>{
+    it('First Test case',()=>{
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    })
+
+    it('Second testcase',()=>{
+        
+    })
+
+    
+})  
