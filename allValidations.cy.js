1d816e952ab5102a6a5249bff856ea6488270a93
Samuel <samuelmwaura081@gmail.com>
2023-07-31 18:42:03 +0300

    [ADD] Validations Practice

---
+++ allValidations.cy.js
@@ -0,0 +1,7 @@
+/// <reference types = "cypress"/>
+describe('Practise on all the validations',()=>{
+    it("Is Practise for all the validations",()=>{
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        
+    })
+})
