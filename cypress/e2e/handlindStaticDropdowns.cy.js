1ba2242ff34c9e6737ccf68166b4bea73e198de9
Samuel <samuelmwaura081@gmail.com>
2023-08-02 19:04:04 +0300

    [ADD] Dynamic Dropedowns

--- cypress/e2e/handlindStaticDropdowns.cy.js
+++ cypress/e2e/handlindStaticDropdowns.cy.js
@@ -2,8 +2,19 @@
 
 describe("Handles Static Select",()=>{
     it("Handle Static select",()=>{
+        
+        //Handle Static Dropdown
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
-
         cy.get("select").select('option3').should('have.value','option3') //Select and validate if the correct one was selected.
+
+        //Handle Dynamic Dropdowns
+        cy.get('input[id="autocomplete"]').type('ke')
+        cy.get('#ui-id-1 .ui-menu-item div').each((element,index,elementList)=>{ //Move from parent to child by spacing.Grab the array of options displayed when you suggest.
+            const elementValue = element.text()
+            if(elementValue === 'Kenya'){ //compare with intended value to select
+                cy.wrap(element).click()
+            }
+        })
+
     })
 })
