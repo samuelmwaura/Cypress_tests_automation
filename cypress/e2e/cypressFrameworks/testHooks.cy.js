f6ec77a8e4e2260a71e2db6000e262935e0e405d
Samuel Mwaura <samuelmwaura081@gmail.com>
2023-08-10 02:57:59 +0300

    [ADD] adding class scoped data variable

--- cypress/e2e/cypressFrameworks/testHooks.cy.js
+++ cypress/e2e/cypressFrameworks/testHooks.cy.js
@@ -1,13 +1,16 @@
 ///<reference types ='cypress' />
 describe('Test Hooks',()=>{
-    before(()=>{
-      cy.fixture()
+
+    before(function(){
+      cy.fixture('example.json').then(function(testData){
+        this.testData = testData
+      })
     })
 
-   it('Tests Hooks',()=>{
+   it('Tests Hooks',function()  {
     cy.visit('https://rahulshettyacademy.com/angularpractice/')
-    cy.get('input[name="name"]:nth-child(2)').type('Sam')
-    cy.get('select').select('Male')
+    cy.get('div.form-group:nth-child(1) input.form-control[name="name"]').type(this.testData.name)
+    cy.get('select').select(this.testData.gender)
   
    })     
 })
@@ -17,3 +20,5 @@ describe('Test Hooks',()=>{
 //cy.fixture helps ypu communicate with the files stored in thf ficture folder.
 
 //One decribe may have mulitiple it blocks.
+
+//SPECIAL NOTE - When "this" is used, the format of functions 'function(){} should be used instead of arrow function otherwise the 'this' is not contextualized.
